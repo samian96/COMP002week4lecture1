4f4df60aedc842713f011d0e85c2cde528ce04aa
samian <sam.iannazzo@yahoo.com>
2025-11-05 23:12:42 -0800

    added let mygrade array / console.log 0 4

---
+++ script.js
@@ -0,0 +1,11 @@
+let myGrade = [50, 30, 70, 100, 27]; // an array of 5 different numbers
+
+console.log(myGrade[0]); // outputs first value of the array, always starts at 0
+console.log(myGrade[4]); // outputs last value in the array
+
+
+
+
+
+
+
