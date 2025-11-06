ae3aac7e171d22674cd32afa857ef772c4cfbcaf
samian <sam.iannazzo@yahoo.com>
2025-11-05 23:16:49 -0800

    added for loop to display array

--- script.js
+++ script.js
@@ -3,7 +3,15 @@ let myGrade = [50, 30, 70, 100, 27]; // an array of 5 different numbers
 console.log(myGrade[0]); // outputs first value of the array, always starts at 0
 console.log(myGrade[4]); // outputs last value in the array
 
+// arrays always start at zero, for each part of the array we go up a number value
 
+/****************************************/ 
+
+// for loop to display all numbers in the array as well as their positions
+
+for(let i = 0; i < myGrade.length; i++){ 
+    console.log(`myGrade[${i}]; ${myGrade[i]}`);
+}
 
 
 
