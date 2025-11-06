a40adba10dc57ac7b0380152ffbae2ff683f92b6
samian <sam.iannazzo@yahoo.com>
2025-11-05 23:25:23 -0800

    added 5 push methods and 3 pop methods

--- script.js
+++ script.js
@@ -13,6 +13,25 @@ for(let i = 0; i < myGrade.length; i++){
     console.log(`myGrade[${i}]; ${myGrade[i]}`);
 }
 
+/**************************************** */
+
+// using push adds a new number to the end of the array
+myGrade.push(88);
+myGrade.push(70);
+myGrade.push(0);
+myGrade.push(99);
+myGrade.push(57);
+console.log(myGrade);
+
+// using the pop method takes away the last number added to the array
+
+myGrade.pop()
+myGrade.pop()
+myGrade.pop()
+console.log(myGrade);
+/***************************************** */
+
+
 
 
 
